cc9444ac95ea45a2efef06f0bb3330a7444fe9e0
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-11 09:16:14 -0600

    adds api base

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const base_url = 'https://api.rawg.io/api/';
+
+const getCurrentMonth = () => {
+  const month = new Date().getMonth();
+  if (month < 10) {
+    reutn`0${month}`;
+  } else {
+    return month;
+  }
+};
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    reutn`0${day}`;
+  } else {
+    return day;
+  }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+const popular_games =
+  'games?dates=${lastYear}, ${currentDate}&ordering=-rating&page_size=10';
+
+export const popularGamesUrl = () => `${base_url}${popularGames}`;
